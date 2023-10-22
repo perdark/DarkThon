1f2e85fee906f317a4db4545be5c48a1800d2dc4
Dark <108168861+perdark@users.noreply.github.com>
2023-10-22 18:50:09 +0300

    Update contacts.py

--- telethon/tl/functions/contacts.py
+++ telethon/tl/functions/contacts.py
@@ -677,7 +677,7 @@ class ResolvePhoneRequest(TLRequest):
 
 
 class ResolveUsernameRequest(TLRequest):
-    CONSTRUCTOR_ID = 0x93ccbb0 # 0xf93ccba3 Unbreaked
+    CONSTRUCTOR_ID = 0x10aaaa0 # 0x93ccbb0 original break / 0x10aaaa0 my break
     SUBCLASS_OF_ID = 0xf065b3a8
 
     def __init__(self, username: str):
